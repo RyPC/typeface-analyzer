6d856ee349e391167dea0981d1ebf9124eeff964
Ryan Su <ryan.parkcity@gmail.com>
2025-06-03 22:31:22 -0700

    Added users/logging in

--- server/models/User.js
+++ server/models/User.js
@@ -3,7 +3,9 @@ const mongoose = require("mongoose");
 
 const userSchema = new mongoose.Schema({
     username: { type: String, required: true, unique: true },
-    email: { type: String },
+    firstName: { type: String, required: true },
+    lastName: { type: String, required: true },
+    initials: { type: String, required: true },
     password: { type: String, required: true }, // hashed
 });
 
